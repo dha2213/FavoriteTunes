de671bd6e8d43da98a95ddfe2afd5d210d31dc32
Dhananjay <dhananjayyadav221303@gmail.com>
2024-01-09 11:29:02 +0530

    added database file

---
+++ FavoriteTunes/__pycache__/models.py
@@ -0,0 +1,15 @@
+# TuneTracker/models.py
+from django.db import models
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Song(models.Model):
+    title = models.CharField(max_length=200)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
